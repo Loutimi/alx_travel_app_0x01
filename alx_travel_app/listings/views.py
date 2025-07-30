d9899552ffa6b803120010b47d498f10bf82ccfa
Loutimi <loutimi59@gmail.com>
2025-07-30 20:21:22 +0100

    Create ListingViewSet, ReviewViewSet, and BookingViewSet

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Listing, Booking
-from .serializers import ListingSerializer, BookingSerializer
+from .models import Listing, Booking, Review
+from .serializers import ListingSerializer, BookingSerializer, ReviewSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import PermissionDenied
 
 
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
 
+
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Booking.objects.all()
+        return Booking.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.user != self.request.user:
+            raise PermissionDenied("You can only edit your own review.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise PermissionDenied("You can only delete your own review.")
+        instance.delete()
