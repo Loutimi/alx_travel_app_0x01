b152de64b772b68db03c4d74b786e6c4b5fd6dad
Loutimi <loutimi59@gmail.com>
2025-07-30 19:18:56 +0100

    Use routers to configure URLs for the API endpoints.

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'Listing', ListingViewSet)
+router.register(r'Booking', BookingViewSet)
 
 urlpatterns = [
-    
+    path('api/', include(router.urls)),
 ]
